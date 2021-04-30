4ee7dfa819724b1db0386d1262434af9608aa475
Wilmer Andersson <wilmer.andersson2@elev.ga.ntig.se>
2021-04-30 19:26:34 +0200

    forts. javascript

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-let incomeListItems = ["Månadslön"];
-let expensesListItems = ["Hyra"];
-let listInput = document.querySelector("[list-input]");
+let incomeListItems = [];
+let expenseListItems = [];
+let inputName = document.querySelector("[list-input-name]");
+let inputAmount = document.querySelector("[list-input-amount]");
 let listForm = document.querySelector("[list-form]");
-let listContainer = document.querySelector("#list-container");
+let incomeListContainer = document.querySelector("#income-list-container");
+let expenseListContainer = document.querySelector("#expenses-list-container");
+
+let incomeButton = document.querySelector("#income-button");
+incomeButton.classList.add("income-button");
+incomeButton.addEventListener("click", incomeOrExpense);
+let expenseButton = document.querySelector("#expense-button");
+expenseButton.classList.add("expense-button");
+expenseButton.addEventListener("click", incomeOrExpense);
+
+function incomeOrExpense (event) {
+    let targetButton = event.target;
+    if (targetButton.classList.contains("income-button")){
+        targetButton.classList.add("income-item");
+        targetButton.classList.remove("expense-item");
+    }
+    if (targetButton.classList.contains("expense-button")){
+        targetButton.classList.add("expense-item");
+        targetButton.classList.remove("income-item");
+    }
+}
 
 listForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (listInput.value.trim() === "") {
+    let nameAmountObject = {
+        id: Date.now(),
+        name: document.getElementById("list-input-name").value,
+        amount: document.getElementById("list-input-amount").value
+    }
+    console.log(nameAmountObject);
+    if (nameAmountObject.name.trim() === "" || nameAmountObject.amount.trim() === "") {
         return;
     }
-    incomeListItems.push(listInput.value.trim());
+    if (incomeButton.classList.contains("income-item")){
+        incomeListItems.push(nameAmountObject.name.trim() + ": " + nameAmountObject.amount.trim());
+    }
+    if (incomeButton.classList.contains("expense-item")){
+        expenseListItems.push(nameAmountObject.name.trim() + ": " + nameAmountObject.amount.trim());
+    }
     updateList();
-    listInput.value = "";
+    document.querySelector('form').reset();
 });
 
 function createList(items) {
-    let incomeList = document.createElement("ul");
+    let itemList = document.createElement("ul");
     items.forEach((item) => {
         let newListItem = document.createElement("li");
         newListItem.innerText = item;
-        newListItem.classList.add("todo-text");
-        incomeList.append(newListItem);
+        newListItem.classList.add("object-text");
+        itemList.append(newListItem);
     });
-    return incomeList;
+    return itemList;
 }
 
 function updateList() {
-    listContainer.innerHTML = "";
-    listContainer.append(createList(incomeListItems));
+    incomeListContainer.innerHTML = "";
+    expenseListContainer.innerHTML = "";
+    incomeListContainer.append(createList(incomeListItems));
+    expenseListContainer.append(createList(expenseListItems));
 }
 
 updateList();
